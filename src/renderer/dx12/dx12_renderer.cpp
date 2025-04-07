c6f22b7e7ff482978542001f1e526b39e7efc207
Alexandra Vabnits <sashaki02@gmail.com>
2025-04-07 23:20:28 +0300

    Lab 07

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -385,7 +385,17 @@ void cg::renderer::dx12_renderer::load_assets()
 
 	// TODO Lab: 3.04
 
-	// TODO Lab: 3.07 Create a fence and fence event
+	// TODO Lab: 3.07
+	THROW_IF_FAILED(command_list->Close());
+
+	THROW_IF_FAILED(device->CreateFence(
+			0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
+	fence_event = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+	if (fence_event == nullptr) {
+		THROW_IF_FAILED(HRESULT_FROM_WIN32(GetLastError()));
+	}
+
+	wait_for_gpu();
 }
 
 
@@ -429,12 +439,25 @@ void cg::renderer::dx12_renderer::populate_command_list()
 
 void cg::renderer::dx12_renderer::move_to_next_frame()
 {
-	// TODO Lab: 3.07 Implement `move_to_next_frame` method
+	// TODO Lab: 3.07
+	const UINT64 current_fence_value = fence_values[frame_index];
+	THROW_IF_FAILED(command_queue->Signal(fence.Get(), current_fence_value));
+	frame_index = swap_chain->GetCurrentBackBufferIndex();
+	if (fence->GetCompletedValue() < fence_values[frame_index]) {
+		THROW_IF_FAILED(fence->SetEventOnCompletion(fence_values[frame_index], fence_event));
+		WaitForSingleObjectEx(fence_event, INFINITE, FALSE);
+	}
+	fence_values[frame_index] = current_fence_value + 1;
 }
 
 void cg::renderer::dx12_renderer::wait_for_gpu()
 {
-	// TODO Lab: 3.07 Implement `wait_for_gpu` method
+	// TODO Lab: 3.07
+	THROW_IF_FAILED(command_queue->Signal(
+		fence.Get(), fence_values[frame_index]));
+	THROW_IF_FAILED(fence->SetEventOnCompletion(fence_values[frame_index], fence_event));
+	WaitForSingleObjectEx(fence_event, INFINITE, FALSE);
+	fence_values[frame_index]++;
 }
 
 
