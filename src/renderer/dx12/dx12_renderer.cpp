71a9f94cb782c4c739d7f49aa4fe4bff5d460726
Alexandra Vabnits <sashaki02@gmail.com>
2025-04-07 23:34:03 +0300

    Lab 3.08

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -45,7 +45,14 @@ void cg::renderer::dx12_renderer::destroy()
 
 void cg::renderer::dx12_renderer::update()
 {
-	// TODO Lab: 3.08 Implement `update` method of `dx12_renderer`
+	// TODO Lab: 3.08
+	auto now = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> duration = now - current_time;
+	frame_duration = duration.count();
+	current_time = now;
+
+	cb.mwpMatrix = camera->get_dxm_mvp_matrix();
+	memcpy(constant_buffer_data_begin, &cb, sizeof(cb));
 }
 
 void cg::renderer::dx12_renderer::render()
